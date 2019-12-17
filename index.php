606a784843882e808dfb9895f98a920350b5e55a
Aldo Hernández <44412311+aldohmolina@users.noreply.github.com>
2019-12-17 11:02:30 -0600

    changes on index.php

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
-<?php
-
-echo "Hello World!";
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+      echo "<h1> Hola Mundo </h1>"
+      for($i=0; $i<100; $i++){
+        echo "="
+      }
+      echo "<br><h2> Sitio Creado por Aldo Hernández Molina </h2>"
+      echo "<br>falto el css :V"
+      for($i=0; $i<100; $i++){
+        echo "="
+      }
+     ?>
+  </body>
+</html>
