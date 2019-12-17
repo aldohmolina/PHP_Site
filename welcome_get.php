834e7c969d3c8fce61c0ead20bd7af269262907c
Aldo Hernández <44412311+aldohmolina@users.noreply.github.com>
2019-12-17 11:47:34 -0600

    Update welcome_get.php

--- welcome_get.php
+++ welcome_get.php
@@ -6,8 +6,8 @@
     <title>Sitio Aldo XS </title>
   </head>
   <body>
-      Bienvenido/a al Sitio <?php echo $_GET["name"]; ?><br>
-      Tu Email es: <?php echo $_GET["email"]; ?><br>
+      Bienvenido/a al Sitio <strong><?php  echo $_GET["name"]; ?></strong><br>
+      Tu Email es: <strong><?php echo $_GET["email"]; ?></strong><br>
     <?php
       //echo "<h1> Hola Mundo </h1>";
       for($i=0; $i<100; $i++){
