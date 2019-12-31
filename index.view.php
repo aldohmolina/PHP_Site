73e37fd47ad10ab9c01739d37b807fc5318f4fc1
Aldo Hernández <44412311+aldohmolina@users.noreply.github.com>
2019-12-30 18:57:25 -0600

    Update index.view.php

--- index.view.php
+++ index.view.php
@@ -8,11 +8,13 @@
   </head>
   <body>
     <div class="wrap">
-        <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
+        <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
             <input type="text" class="form-control"  id="nombre" name="nombre" placeholder="Nombre: "value="">
-            <input type="text" class="form-control" id="correo" name="correo" placeholder="Correo: "value="">
 
-            <textarea name="mensaje" class="form-control" id="mensaje" placeholder="Mensaje"></textarea>
+            <input type="text" class="form-control" id="correo" name="correo" placeholder="Correo: "value="">
+            
+            <textarea name="mensaje" class="form-control" id="mensaje" placeholder="Mensaje:"></textarea>
+
             <input type="submit" name="submit" class="btn btn-primary" value="Enviar Correo">
         </form>
     </div>
