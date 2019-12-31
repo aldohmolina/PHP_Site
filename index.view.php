a680d505c081305e3ee6c05da6d20c0db9d7b0a8
Aldo Hernández <44412311+aldohmolina@users.noreply.github.com>
2019-12-30 18:40:09 -0600

    Update index.view.php

--- index.view.php
+++ index.view.php
@@ -1,1 +1,11 @@
-
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="estilos.css">
+    <title>Formulario Contacto</title>
+  </head>
+  <body>
+    
+  </body>
+</html>
