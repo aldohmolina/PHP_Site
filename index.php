61fc892f54cf45f2f7295a3401f7f7fcadfef45f
Aldo Hernández <44412311+aldohmolina@users.noreply.github.com>
2019-12-31 13:04:44 -0600

    Update index.php

--- index.php
+++ index.php
@@ -40,7 +40,42 @@ if (isset($_POST['submit'])) {
       $mensaje_preparado .= "Mail: $correo \n";
       $mensaje_preparado .= "Message: " . $mensaje;
 
-      mail($enviar_a, $asunto, $mensaje_preparado);
+      //mail($enviar_a, $asunto, $mensaje_preparado);
+      $url = 'https://api.sendgrid.com/';
+      $user = 'azure_d7568cfda87fb8c3a865cafca699db03@azure.com';
+      $pass = 'H3rn4nd3z';
+
+      $params = array(
+          'api_user' => $user,
+          'api_key' => $pass,
+          'to' => 'aldohmolina@gmail.com',
+          'subject' => 'testing from curl',
+          'html' => 'testing body_html',
+          'text' => 'testing body',
+          'from' => 'anna@contoso.com',
+      );
+
+    $request = $url.'api/mail.send.json';
+
+    // Generate curl request
+    $session = curl_init($request);
+
+    // Tell curl to use HTTP POST
+    curl_setopt ($session, CURLOPT_POST, true);
+
+    // Tell curl that this is the body of the POST
+    curl_setopt ($session, CURLOPT_POSTFIELDS, $params);
+
+    // Tell curl not to return headers, but do return the response
+    curl_setopt($session, CURLOPT_HEADER, false);
+    curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+
+ // obtain response
+ $response = curl_exec($session);
+ curl_close($session);
+
+ // print everything out
+ print_r($response);
       $enviado = 'true';
     }
 }
