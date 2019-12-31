5bc60f765cfd284ce5e94817e419da5842dd8020
Aldo Hernández <44412311+aldohmolina@users.noreply.github.com>
2019-12-31 11:52:10 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
 <?php 
+
+$errores = '';
+$enviado = '';
+
+if (isset($_POST['submit'])) {
+    $nombre = $_POST['nombre'];
+    $correo = $_POST['correo'];
+    $mensaje = $_POST['mensaje'];
+
+    if(!empty($nombre)){
+      $nombre = trim($nombre);
+      $nombre = filter_var($nombre,FILTER_SANITIZE_STRING);
+    } else {
+      $errores .= 'Por favor ingresa un nombre <br />';
+    }
+    
+}
+
 require 'index.view.php';
 ?>
