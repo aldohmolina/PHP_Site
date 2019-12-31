ab5d9d739bfe028e421e5ad0c5457de6904ea0bf
Aldo Hernández <44412311+aldohmolina@users.noreply.github.com>
2019-12-30 18:37:01 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,3 @@
-<html>
-  <head>
-    <title> login </title>
-    <link rel="stylesheet" type="text/css" href="main.css">
-  </head>
-  <body>
-    <h2> Bienvenido al sitio </h2>
-    <h3> Por favor logeate </h3>
-    
-  <form action="welcome_get.php" method="post">
-    Nombre:   <input type="text" name="name"><br><br>
-    E-mail:   <input type="text" name="email"><br><br>
-  <input type="submit">
-  </form>
-
-</body>
-</html>
+<?php 
+require 'index.view.php';
+?>
